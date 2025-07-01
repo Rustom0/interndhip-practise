03cc75fecac98e7e5c621bb3398755edcc1a98f3
Rustom0 <sahilshrestha@icloud.com>
2025-07-01 13:23:59 +0300

    added the extra feature customer model

--- src/components/CustomerView.js
+++ src/components/CustomerView.js
@@ -12,6 +12,7 @@ import React, { useEffect, useState } from "react";
 */
 
 import Loader from "./Loader";
+import CustomerModal from "./CustomerModel";
 
 /*
 |--------------------------------------------------
@@ -24,7 +25,16 @@ const CustomerView = () => {
   const [loading, setLoading] = useState(false); // State to manage loading state
   const [page, setPage] = useState(1); // State to manage pagination
 
-/*
+  /*
+  |--------------------------------------------------
+  | State to manage selected customer and modal visibility
+  |--------------------------------------------------
+  */
+
+  const [selectedCustomer, setSelectedCustomer] = useState(null);
+  const [showModal, setShowModal] = useState(false);
+
+  /*
 |--------------------------------------------------
 | functions to fetch and manage customer data from API
 |--------------------------------------------------
@@ -36,7 +46,7 @@ const CustomerView = () => {
         "https://fakerapi.it/api/v2/persons?_quantity=10"
       );
       const data = await response.json();
-    /*
+      /*
     |--------------------------------------------------
     | // Set the customer data to state easy to use in the UI 
     |--------------------------------------------------
@@ -49,7 +59,7 @@ const CustomerView = () => {
       setLoading(false); // Set loading state to false after fetching
     }
   };
-    /*
+  /*
     |--------------------------------------------------
     | fetchCustomers function is called when the component mounts
     |--------------------------------------------------
@@ -90,28 +100,49 @@ const CustomerView = () => {
   };
   /*
   |--------------------------------------------------
+  | Function to handle customer click to open modal
+  |--------------------------------------------------
+  */
+  const handleCustomerClick = (customer) => {
+    setSelectedCustomer(customer);
+    console.log("Selected Customer:", customer);
+    setShowModal(true);
+  };
+
+  /*
+  |--------------------------------------------------
+  | Function to handle modal close
+  |--------------------------------------------------
+  */
+  const handleModalClose = () => {
+    setShowModal(false);
+    setSelectedCustomer(null);
+  };
+  /*
+  |--------------------------------------------------
   | useEffect to clear message after 3 seconds
   |--------------------------------------------------
   */
 
   useEffect(() => {
-  if (message) {
-    const timer = setTimeout(() => {
-      setMessage('');
-    }, 2000);
-    return () => clearTimeout(timer);
-  }
-}, [message]);
-/*
+    if (message) {
+      const timer = setTimeout(() => {
+        setMessage("");
+      }, 2000);
+      return () => clearTimeout(timer);
+    }
+  }, [message]);
+
+  /*
 |--------------------------------------------------
 |styling the component
 |--------------------------------------------------
 */
-const tableStyle = {
-  color: "rgb(98, 92, 141)", 
-  fontWeight: "LIGHT",
-  background: "rgb(248, 251, 255)" 
-};
+  const tableStyle = {
+    color: "rgb(98, 92, 141)",
+    fontWeight: "LIGHT",
+    background: "rgb(248, 251, 255)",
+  };
 
   return (
     <div className="container mt-4">
@@ -120,27 +151,45 @@ const tableStyle = {
         <div className="d-flex">
           <input
             type="text"
-            className="form-control me-2 bg-light form fa-solid fa-search " style={{ width: '320px', height: '30px' } }
+            className="form-control me-2 bg-light form fa-solid fa-search "
+            style={{ width: "320px", height: "30px" }}
             placeholder="Search transactions"
           />
-          <button onClick={fetchCustomers} className="btn btn-primary btn-sm" style={{ width: '80px',}} >
+          <button
+            onClick={fetchCustomers}
+            className="btn btn-primary btn-sm"
+            style={{ width: "80px" }}
+          >
             Refresh
           </button>
-          <button className="btn ms-2 btn-sm ">
-            View All
-          </button>
+          <button className="btn ms-2 btn-sm ">View All</button>
         </div>
       </div>
       {message && <div className="alert alert-success">{message}</div>}
-      <table className="table" style={{borderBottom: "1px solid rgb(243, 240, 240)"}}>
+      <table
+        className="table"
+        style={{ borderBottom: "1px solid rgb(243, 240, 240)" }}
+      >
         <thead>
           <tr>
-            <th className="text-start" style={tableStyle}>Customer Name</th>
-            <th className="text-start" style={tableStyle}>Email</th>
-            <th className="text-start"  style={tableStyle}>Loan Type</th>
-            <th className="text-start"  style={tableStyle}>Outstanding Blc</th>
-            <th className="text-start"  style={tableStyle}>Status</th>
-            <th className="text-start"  style={tableStyle}>Action</th>
+            <th className="text-start" style={tableStyle}>
+              Customer Name
+            </th>
+            <th className="text-start" style={tableStyle}>
+              Email
+            </th>
+            <th className="text-start" style={tableStyle}>
+              Loan Type
+            </th>
+            <th className="text-start" style={tableStyle}>
+              Outstanding Blc
+            </th>
+            <th className="text-start" style={tableStyle}>
+              Status
+            </th>
+            <th className="text-start" style={tableStyle}>
+              Action
+            </th>
           </tr>
         </thead>
         <tbody>
@@ -153,11 +202,21 @@ const tableStyle = {
           ) : (
             Array.isArray(customers) &&
             customers.map((customer) => (
-              <tr key={customer.id}>
-                <td className="text-start">{customer.firstname} {customer.lastname}</td>
+              <tr
+                key={customer.id}
+                onClick={() => handleCustomerClick(customer)} // CHANGED
+                style={{ cursor: "pointer" }}
+              >
+                <td className="text-start">
+                  {customer.firstname} {customer.lastname}
+                </td>
                 <td className="text-start">{customer.email}</td>
-                <td className="text-start" >{customer.loanType === "female" ? "paid" : "Ongoing"}</td>
-                <td className="text-start">{customer.gender === "female" ? "N10.000" : "N20.000"}</td>
+                <td className="text-start">
+                  {customer.loanType === "female" ? "paid" : "Ongoing"}
+                </td>
+                <td className="text-start">
+                  {customer.gender === "female" ? "N10.000" : "N20.000"}
+                </td>
                 <td className="text-start">
                   <span
                     className={`badge  ${
@@ -170,7 +229,10 @@ const tableStyle = {
                 <td className="text-start">
                   <button
                     className="btn btn-danger btn-sm"
-                    onClick={() => deleteCustomer(customer.id)}
+                    onClick={(e) => {
+                      e.stopPropagation(); // NEW – stop row click
+                      deleteCustomer(customer.id);
+                    }}
                   >
                     Delete
                   </button>
@@ -180,16 +242,19 @@ const tableStyle = {
           )}
         </tbody>
       </table>
-      
+
       {!loading && customers.length === 0 && (
         <div className="alert alert-info text-center">
-          No customers found.{' '}
-          <button onClick={fetchCustomers} className="btn btn-sm btn-primary ms-2">
+          No customers found.{" "}
+          <button
+            onClick={fetchCustomers}
+            className="btn btn-sm btn-primary ms-2"
+          >
             Refresh
           </button>
-          </div>
-        )}
-        <div className="conatiner text-center  ">
+        </div>
+      )}
+      <div className="conatiner text-center  ">
         <button
           disabled={page <= 1}
           type="button"
@@ -206,6 +271,12 @@ const tableStyle = {
           Next &rarr;
         </button>
       </div>
+      <CustomerModal
+        key={selectedCustomer?.id}
+        customer={selectedCustomer}
+        visible={showModal}
+        onClose={handleModalClose}
+      />
     </div>
   );
 };
